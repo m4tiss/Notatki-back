2b7390a0b419945cbddf800cf2716af3d1841466
m4tiss <mateusz039@op.pl>
2023-06-06 18:43:48 +0200

    save,get,update,delete

--- actions/api/noteActions.js
+++ actions/api/noteActions.js
@@ -1,30 +1,44 @@
 const Note = require("../../db/models/note");
 
 class NoteActions {
-  saveNote(req, res) {
-    // const newNote = new Note({
-    //   title: "ztestować",
-    //   body: "dzisiaj",
-    // });
-    // newNote.save().then(() => {
-    //   console.log("zapisana");
-    // });
+   async saveNote(req, res) {
+
     const title = req.body.title;
     const body = req.body.body;
-    res.send("tytuł: " + title+"body: "+ body);
+    let note;
+    try{
+      note = new Note({title,body});
+      await note.save();
+    }catch(err){
+      return res.status(422).json({message:err.message})
+    }
+    res.status(201).json(note);
   }
-  getAllNotes(req, res) {
-    res.send("api działa!");
+  async getAllNotes(req, res) {
+    const doc =  await Note.find({});
+    res.status(200).json(doc);
   }
-  getNote(req, res) {
-    res.send("info o notatce");
-  }
-  updateNote(req, res) {
-    res.send("update się udał");
-  }
-  deleteNote(req, res) {
+  async getNote(req, res) {
     const id = req.params.id;
-    res.send("usunięcie sie udało ID: " + id);
+    const note = await Note.findOne({_id:id});
+    res.status(200).json(note);
+  }
+  async updateNote(req, res) {
+    const id = req.params.id;
+    const title = req.body.title;
+    const body = req.body.body;
+
+     const note = await Note.findOne({_id:id})
+     note.title = title;
+     note.body = body;
+     await note.save();
+
+    res.status(201).json(note);
+  }
+   async deleteNote(req, res) {
+    const id = req.params.id;
+    await Note.deleteOne({_id:id});
+    res.sendStatus(204);
   }
 }
 
