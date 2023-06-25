74bffe896cb90f482206d4047b0f8aa72dfbe5d2
m4tiss <mateusz039@op.pl>
2023-06-25 11:09:22 +0200

    Connect Frontend with Backend

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const { port } = require('./config');
 const apiRouter = require('./routes/api');
 const bodyParser = require('body-parser');
-
+const cors = require('cors');
 
 //db
 require('./db/mongoose');
@@ -12,6 +12,8 @@ require('./db/mongoose');
 //parsery
 app.use(bodyParser.json());
 
+//fix cors
+app.use(cors());
 // routes
 app.use('/api/', apiRouter);
 
